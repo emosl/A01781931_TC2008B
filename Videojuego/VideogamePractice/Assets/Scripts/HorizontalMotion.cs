4f55d72d87a710cfb95967eb28c42c959f99dfab
Emilia Salazar <emiliasalazar67@gmail.com>
2023-03-01 12:26:08 -0600

    horizontra

--- Videojuego/VideogamePractice/Assets/Scripts/HorizontalMotion.cs
+++ Videojuego/VideogamePractice/Assets/Scripts/HorizontalMotion.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class HorizontalMotion : MonoBehaviour
 {
+    [SerializeField] float speed;
+    [SerializeField] float limit;
+
+    Vector3 move;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,19 @@ public class HorizontalMotion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        move.x=Input.GetAxis("Horizontal");
+
+       // Debug.Log("H motion: " +move.x);
+
+        //Limit the movement to a specific range of coordinates 
+        if(transform.position.x < -limit && move.x < 0){
+            move.x = 0;
+        } else if (transform.position.x > limit && move.x > 0){
+            move.x = 0;
+        }
+
+        transform.Translate(move * speed * Time.deltaTime);
+
+
     }
 }
